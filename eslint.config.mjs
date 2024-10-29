21f62305ad728999ab017269157f3270bb5af741
Ben Tomsett <ben@tomsett.xyz>
2024-10-29 23:55:56 +0000

    Add ESLint with new flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginTailwind from 'eslint-plugin-tailwindcss';
+
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname
+      }
+    }
+  },
+  {
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  },
+  {
+    ignores: [
+        '*.config.js'
+    ]
+  },
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  pluginReact.configs.flat.recommended,
+  ...pluginTailwind.configs['flat/recommended'],
+];
